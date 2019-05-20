372b1d4d2f8a44d3b8e0ebffa948388c18cd664a
Vincil Bishop <vbishop@surgeforward.com>
2019-05-20 10:22:24 -0500

    Updated for more material react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-import NavBar from './components/NavBar'
+// import NavBar from './components/NavBar'
+import Dashboard from './Dashboard'
 class App extends Component {
   render () {
     return (
       <div>
-        <NavBar />
+        <Dashboard />
       </div>
     )
   }
