ec13d3345e2be7ac97900ccb1e97518149f26525
vsurge <vbishop@surgeforward.com>
2019-05-24 09:30:00 -0500

    Added Firebase integration (#4)
    * Updated for neutron
    
    * Removed node from .gitignore
    
    * Adde Material UI for react
    
    * Updated for readme
    
    * Updated for material UI & NavBar
    
    * Updated for more material react
    
    * Updated for firebase integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
 // import NavBar from './components/NavBar'
 import Dashboard from './Dashboard'
+import DataService from './services/DataService'
+
 class App extends Component {
+  constructor (props) {
+    super(props)
+
+    DataService.write()
+    DataService.read()
+  }
+
   render () {
     return (
       <div>
---
+++ src/services/DataService.jsx
@@ -0,0 +1,51 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from 'firebase/app'
+
+// Add the Firebase products that you want to use
+import 'firebase/auth'
+import 'firebase/firestore'
+
+class DataService {
+  static db
+
+  static init () {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyCDJo-Q-tZyBm5ggcHKAeKU4s8HSfdljbI',
+      authDomain: 'materialreact512.firebaseapp.com',
+      databaseURL: 'https://materialreact512.firebaseio.com',
+      projectId: 'materialreact512',
+      storageBucket: 'materialreact512.appspot.com',
+      messagingSenderId: '924935119837',
+      appId: '1:924935119837:web:f7dc79ec450117a2'
+    }
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig)
+
+    this.db = firebase.firestore()
+  }
+
+  static read () {
+    this.db.collection('users').get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} =>`, doc.data())
+      })
+    })
+  }
+
+  static write () {
+    this.db.collection('users').add({
+      first: 'Ada',
+      last: 'Lovelace',
+      born: 1815
+    })
+      .then(function (docRef) {
+        console.log('Document written with ID: ', docRef.id)
+      })
+      .catch(function (error) {
+        console.error('Error adding document: ', error)
+      })
+  }
+}
+
+export default DataService
